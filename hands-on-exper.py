905c5ff4d147a42f9f1fe9a0aae1eca5b53e29dc
Nugget2468 <christianwinnitzk@gmail.com>
2024-06-28 17:39:52 -0400

    updated code

---
+++ hands-on-exper.py
@@ -0,0 +1,8 @@
+#If one cake requires 250 grams of flour and yuou have 2.5 kg of flour, how many cakes can you make?
+
+flour_per_cake = 250
+total_flour = 2.5 * 1000
+number_of_cakes = total_flour // flour_per_cake
+
+print(number_of_cakes)
+
